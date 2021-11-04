e6b38f9caf73b3966c09494e978d928213bf66c1
Krishna <krishnasonune87@gmail.com>
2021-11-04 23:41:09 +0530

    make it better

--- bot.py
+++ bot.py
@@ -10,14 +10,14 @@ bot = Bot(TOKEN)
 print(bot.get_me())
 
 def start(update:Update, context:CallbackContext):
-    update.message.reply_text('your starting message')
+    update.message.reply_text("hii i am jokify bot, if you are having bad day, I'll try to make laugh with my jokes just give /joke command for jokes and /help for any kind of help")
 
 def joke(update:Update, context:CallbackContext):
     joke = pyjokes.get_joke()
     update.message.reply_text(joke)
 
 def help(update:Update, context:CallbackContext):
-    update.message.reply_text('your help message')
+    update.message.reply_text('If you like my jokify bot pleease make sure you share it with your friends and spread smile, and for any help contact my owner on PyDeveloper')
 
 updater = Updater(TOKEN, use_context=True)
 dispatch = updater.dispatcher
